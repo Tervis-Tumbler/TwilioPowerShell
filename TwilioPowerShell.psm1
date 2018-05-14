e82449b2cb02ef0bd6461ba2d52c03b60f70fc0b
fhoseinih <hosseini@tervis.com>
2018-05-14 16:13:11 -0400

    Added Add,Remove, and Update-TwilioAddrresses, Set-TwilioIncomingPhoneNumber, Get-TwilioIncomingPhoneNumbers, and Get-TwilioAddresses

--- TwilioPowerShell.psm1
+++ TwilioPowerShell.psm1
@@ -47,21 +47,96 @@ function New-TwilioCall {
     Invoke-TwilioAPIFunction -HttpMethod post -Resource Accounts -SubResource Calls -Body $($PSBoundParameters | ConvertTo-URLEncodedQueryStringParameterString)
 }
 
-Function Get-TwilioCalls {
+function Get-TwilioCalls {
     Invoke-TwilioAPIFunction -HttpMethod get -Resource Accounts -SubResource Calls
 }
 
-Function Get-TwilioIncomingPhoneNumbers {
+function Get-TwilioIncomingPhoneNumbers {
     Invoke-TwilioAPIFunction -HttpMethod get -Resource Accounts -SubResource IncomingPhoneNumbers
 }
 
-Function Invoke-TwilioAPIFunction {
+function Get-TwilioAddresses {
+    Invoke-TwilioAPIFunction -HttpMethod get -Resource Accounts -SubResource Addresses
+}
+
+function Add-TwilioAddresses {
+    param(
+        [String]$FriendlyName,
+        [Parameter(Mandatory)][String]$CustomerName,
+        [Parameter(Mandatory)][String]$Street,
+        [Parameter(Mandatory)][String]$City,
+        [Parameter(Mandatory)][String]$Region,
+        [Parameter(Mandatory)][String]$PostalCode,
+        [Parameter(Mandatory)][String]$IsoCountry
+        
+    )
+    Invoke-TwilioAPIFunction -HttpMethod post -Resource Accounts -SubResource Addresses -Body $($PSBoundParameters | ConvertTo-URLEncodedQueryStringParameterString)
+}
+
+function Remove-TwilioAddresses {
+    param(
+        [Parameter(Mandatory)]$SubResourceSid
+    )
+
+    Invoke-TwilioAPIFunction -HttpMethod delete -Resource Accounts -SubResource Addresses -SubResourceSid $SubResourceSid -Body $($PSBoundParameters | ConvertTo-URLEncodedQueryStringParameterString)
+
+}
+
+function Update-TwilioAddresses {
+    param(
+        [Parameter(Mandatory)]$SubResourceSid,
+        $CustomerName,
+        $FriendlyName,
+        $Street,
+        $street_secondary,
+        $City,
+        $Region,
+        $PostalCode,
+        [Parameter][ValidateSet("true","false")]$AutoCorrectAddress
+    )
+
+    Invoke-TwilioAPIFunction -HttpMethod post -Resource Accounts -SubResource Addresses -SubResourceSid $SubResourceSid -Body $($PSBoundParameters | ConvertTo-URLEncodedQueryStringParameterString)
+}
+
+function Set-TwilioIncomingPhoneNumber {
+    param (
+       [Parameter(Mandatory)][string]$FriendlyName,
+       $ApiVersion,
+       $VoiceUrl,
+       $VoiceMethod,
+       $VoiceFallbackUrl,
+       $VoiceFallbackMethod,
+       $StatusCallback,
+       $StatusCallbackMethod,
+       $VoiceCallerIdLookup,
+       $VoiceApplicationSid,
+       $TrunkSid,
+       $SmsUrl,
+       $SmsMethod,
+       $SmsFallbackUrl,
+       $SmsFallbackMethod,
+       $SmsApplicationSid,
+       $AccountSid, 
+       $AddressSid
+    )
+    $SubResourceSid = Get-TwilioIncomingPhoneNumbers | 
+                Select-Object -ExpandProperty incoming_phone_numbers | 
+                Where-Object friendly_name -eq $FriendlyName |
+                Select-Object -ExpandProperty sid
+
+    Invoke-TwilioAPIFunction -HttpMethod post -Resource Accounts -SubResource IncomingPhoneNumbers -SubResourceSid $SubResourceSid -Body $(
+                $PSBoundParameters | ConvertTo-URLEncodedQueryStringParameterString
+    )
+}
+
+function Invoke-TwilioAPIFunction {
     param (
         $HttpMethod,
         $Resource,
         $SubResource,
         $Body,
         $Header,
+        $SubResourceSid,
         [Switch]$Debug
     )
     if ($Debug) {
@@ -73,7 +148,11 @@ Function Invoke-TwilioAPIFunction {
     $URI = if (-not $SubResource ) {
         $APIRoot +"/2010-04-01/$Resource.json"
     } else {
-        $APIRoot +"/2010-04-01/$Resource/$($Credential.UserName)/$SubResource.json"
+        if (-not $SubResourceSid ) {
+            $APIRoot +"/2010-04-01/$Resource/$($Credential.UserName)/$SubResource.json?PageSize=350"
+        } else { 
+            $APIRoot +"/2010-04-01/$Resource/$($Credential.UserName)/$SubResource/$SubResourceSid.json"
+        }    
     }
 
     #$Response = if ($Header) {
@@ -83,17 +162,16 @@ Function Invoke-TwilioAPIFunction {
     #}
 
     $Response = if ($Body) {
-        Invoke-RestMethod -Method $HttpMethod -Credential $Credential -Uri $URI -Body $Body
+        Invoke-RestMethod -Method $HttpMethod -Credential $Credential -Uri $URI  -Body $Body
     } else {
         Invoke-RestMethod -Method $HttpMethod -Credential $Credential -Uri $URI
     }
-
+    
     $Response
 }
 
-
 #$TwilioResources = @"
-#Accounts
+#Accountsi
 #Addresses
 #API Keys
 #Applications
